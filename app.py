72ea60d82ee3ca1adfb0fb6d54c0986691c51560
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-07-23 22:54:55 -0400

    Word definition and Error handling complete

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+
+data = json.load(open("data.json"))
+
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please try again."
+
+
+word = input("Enter Word: ")
+
+print(translate(word))
