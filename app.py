9879539a1b9e7428536afe31e410d8b58944e4cd
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-07-28 12:21:54 -0400

    added input for user if similar word is availible

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,6 +8,21 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean " + get_close_matches(w,
+                                                       data.keys())[0] + " instead: Y/N: ")
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+
+        elif yn == "N":
+            return "Ok"
+
+        while yn != "Y" or "N":
+            yn = input("Did you mean " + get_close_matches(w,
+                                                           data.keys())[0] + " instead: Y/N: ")
+            if yn == "Y":
+                return data[get_close_matches(w, data.keys())[0]]
+
     else:
         return "The word doesn't exist. Please try again."
 
